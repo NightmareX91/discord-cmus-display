84b7f158d10e0f3216e5dbfde59704b1491254e6
meanwhile <nightmarex91@gmail.com>
2016-03-21 02:23:00 +0000

    Added colors dependency and authentication

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ catch (e) {
     console.log("You need to run npm install and make sure it passes with no errors!");
     process.exit();
 }
+
+try {
+    var colours = require("colors");
+}
+catch (e) {
+    console.log("You need to run npm install and make sure it passes with no errors!");
+    process.exit();
+}
+
+try {
+    var authDetails = require("./auth.json");
+}
+catch (e) {
+    console.log("You need to make an auth.json using the example file on the github!");
+    process.exit();
+}
+
+var bot = new discord.Client();
+
+bot.on("ready", function() {
+    console.log("Connected!".green);
+});
+
+bot.login(authDetails.username, authDetails.password);
