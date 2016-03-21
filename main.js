047a5e4545845f054c98d376b36a55a9a696766e
meanwhile <nightmarex91@gmail.com>
2016-03-21 09:31:09 +0000

    Added song title check

--- main.js
+++ main.js
@@ -23,9 +23,32 @@ catch (e) {
 }
 
 var bot = new discord.Client();
+var exec = require("child_process").exec;
+var cmd = "cmus-remote -Q";
+var songTitle;
 
 bot.on("ready", function() {
     console.log("Connected!".green);
+
+    setInterval(function() {
+        exec(cmd, function(err, stdout, stderr) {
+            if (err) {
+                throw err;
+            }
+
+            var str1;
+            var str2;
+            var str3;
+
+            str1 = stdout.split("tag title ")[1];
+            str2 = str1.split("tag date ")[0];
+            str3 = str2.replace(/(\r\n|\n|\r)/gm, "");
+
+            songTitle = str3;
+        });
+
+        bot.setPlayingGame(songTitle);
+    }, 500);
 });
 
 bot.on("disconnected", function() {
