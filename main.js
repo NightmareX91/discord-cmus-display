de36537b43d07cf7cf1888bd6fe4effea66b5228
meanwhile <nightmarex91@gmail.com>
2016-03-22 16:22:30 +0000

    Fixed bugs caused by cmus not running or in stopped state

--- main.js
+++ main.js
@@ -34,42 +34,51 @@ bot.on("ready", function() {
     setInterval(function() {
         exec(cmd, function(err, stdout, stderr) {
             if (err) {
-                throw err;
+                if (err.message.includes("cmus is not running")) {
+                    bot.setPlayingGame("cmus: Not running");
+                }
+                else {
+                    throw err;
+                }
             }
+            else if (stdout.includes("status stopped")) {
+                bot.setPlayingGame("cmud: Stopped");
+            }
+            else {
+                // Song title separation
+                var str1;
+                var str2;
+                var str3;
 
-            // Song title separation
-            var str1;
-            var str2;
-            var str3;
+                str1 = stdout.split("tag title ")[1];
+                str2 = str1.split("tag date ")[0];
+                str3 = str2.replace(/(\r\n|\n|\r)/gm, "");
 
-            str1 = stdout.split("tag title ")[1];
-            str2 = str1.split("tag date ")[0];
-            str3 = str2.replace(/(\r\n|\n|\r)/gm, "");
+                songTitle = str3;
 
-            songTitle = str3;
+                // Status separation
+                var str4;
+                var str5;
+                var str6;
 
-            // Status separation
-            var str4;
-            var str5;
-            var str6;
+                str4 = stdout.split("status ")[1];
+                str5 = str4.split("file ")[0];
+                str6 = str5.replace(/(\r\n|\n|\r)/gm, "");
 
-            str4 = stdout.split("status ")[1];
-            str5 = str4.split("file ")[0];
-            str6 = str5.replace(/(\r\n|\n|\r)/gm, "");
-
-            cmusStatus = str6;
+                cmusStatus = str6;
+            }
         });
 
         if (cmusStatus === "stopped") {
-            console.log("cmus status: stopped".yellow);
-            bot.setPlayingGame();
+            //console.log("cmus status: stopped".yellow);
+            bot.setPlayingGame("cmus: Stopped");
         }
         else if (cmusStatus === "paused") {
-            console.log("cmus status: paused".yellow);
+            //console.log("cmus status: paused".yellow);
             bot.setPlayingGame("cmus: Song Paused");
         }
         else if (cmusStatus === "playing") {
-            console.log("cmus status: playing".yellow);
+            //console.log("cmus status: playing".yellow);
             bot.setPlayingGame("cmus: " + songTitle);
         }
     }, 500);
